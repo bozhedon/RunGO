65b1023f175aac22eac7868b8bb35f639ced0449
krieg31 <krieg31@users.noreply.github.com>
2018-11-06 16:06:23 +0900

    123

--- app/src/main/java/com/myrungo/rungo/Challenge.java
+++ app/src/main/java/com/myrungo/rungo/Challenge.java
@@ -1,12 +1,12 @@
 package com.myrungo.rungo;
 
-import android.content.Context;
-import android.net.Uri;
-import android.os.Bundle;
-import android.support.v4.app.Fragment;
-import android.view.LayoutInflater;
-import android.view.View;
-import android.view.ViewGroup;
+        import android.content.Context;
+        import android.net.Uri;
+        import android.os.Bundle;
+        import android.support.v4.app.Fragment;
+        import android.view.LayoutInflater;
+        import android.view.View;
+        import android.view.ViewGroup;
 
 
 public class Challenge extends Fragment {
