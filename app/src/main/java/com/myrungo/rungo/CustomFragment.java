a9e27c1c1c07faa8b25ad998717c370c7cecde2f
bozhedon <bozhedon-yan@yandex.ru>
2018-11-16 19:13:17 +0900

    customization

--- app/src/main/java/com/myrungo/rungo/CustomFragment.java
+++ app/src/main/java/com/myrungo/rungo/CustomFragment.java
@@ -7,14 +7,54 @@ import android.support.v4.app.Fragment;
 import android.view.LayoutInflater;
 import android.view.View;
 import android.view.ViewGroup;
+import android.widget.ImageView;
 
 
 public class CustomFragment extends Fragment {
+    CatView catview;
+    ImageView badcat;
+    ImageView karatecat;
+    ImageView officecat;
+    ImageView sportcat;
     @Override
     public View onCreateView(LayoutInflater inflater, ViewGroup container,
                              Bundle savedInstanceState) {
 
         View view = inflater.inflate(R.layout.fragment_customization, container, false);
+        catview = view.findViewById(R.id.cat);
+        badcat = view.findViewById(R.id.bad_cat_cloth);
+        karatecat = view.findViewById(R.id.karate_cloth);
+        officecat = view.findViewById(R.id.office_cloth);
+        sportcat = view.findViewById(R.id.casual_sport_cloth);
+        badcat.setClickable(true);
+        karatecat.setClickable(true);
+        officecat.setClickable(true);
+        sportcat.setClickable(true);
+        badcat.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View view) {
+                catview.setSkin(CatView.Skins.BAD);
+            }
+        });
+        karatecat.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View view) {
+                catview.setSkin(CatView.Skins.KARATE);
+            }
+        });
+        officecat.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View view) {
+                catview.setSkin(CatView.Skins.BUSINESS);
+            }
+        });
+        sportcat.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View view) {
+                catview.setSkin(CatView.Skins.NORMAL);
+            }
+        });
         return view;
     }
+
 }
