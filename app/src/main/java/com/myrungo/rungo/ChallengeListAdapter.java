6b111536c62d43f19f6a325e5fca1636c265f108
krieg31 <krieg31@users.noreply.github.com>
2018-11-19 03:39:01 +0900

    challenge_in_progress_v2

--- app/src/main/java/com/myrungo/rungo/ChallengeListAdapter.java
+++ app/src/main/java/com/myrungo/rungo/ChallengeListAdapter.java
@@ -2,13 +2,15 @@ package com.myrungo.rungo;
 
 import android.app.Activity;
 import android.content.Context;
+import android.os.Handler;
 import android.view.LayoutInflater;
 import android.view.View;
 import android.view.ViewGroup;
 import android.widget.BaseAdapter;
+import android.widget.TextView;
 
 import com.android.volley.toolbox.ImageLoader;
-import com.myrungo.rungo.ChallengeImageView;
+import com.android.volley.toolbox.NetworkImageView;
 import com.myrungo.rungo.ChallengeItem;
 
 import java.util.List;
@@ -17,10 +19,12 @@ public class ChallengeListAdapter extends BaseAdapter {
     private Activity activity;
     private LayoutInflater inflater;
     private List<ChallengeItem> challengeItems;
+    ImageLoader imageLoader = AppController.getInstance().getImageLoader();
 
-    public ChallengeListAdapter(Activity activity, List<ChallengeItem> feedItems) {
+
+    public ChallengeListAdapter(Activity activity, List<ChallengeItem> challengeItems) {
         this.activity = activity;
-        this.challengeItems = feedItems;
+        this.challengeItems = challengeItems;
     }
 
     @Override
@@ -45,11 +49,23 @@ public class ChallengeListAdapter extends BaseAdapter {
                     .getSystemService(Context.LAYOUT_INFLATER_SERVICE);
         if (convertView == null)
             convertView = inflater.inflate(R.layout.challenge_item, null);
+        if (imageLoader == null)
+            imageLoader = AppController.getInstance().getImageLoader();
+        TextView text_distance= convertView.findViewById(R.id.text_distance);
+        TextView textTime= convertView.findViewById(R.id.textTime);
+        NetworkImageView networkImageView = convertView
+                .findViewById(R.id.challengeImageView);
+        ChallengeItem challengeItem =challengeItems.get(position);
 
+        if (challengeItem.getImge() != null) {
+            networkImageView.setVisibility(View.VISIBLE);
+            networkImageView.setImageUrl(challengeItem.getImge(), imageLoader);
+        } else {
+            networkImageView.setVisibility(View.GONE);
+        }
 
-
-        ChallengeItem item = challengeItems.get(position);
-
+        text_distance.setText(challengeItem.distance.toString());
+        textTime.setText("0"+challengeItem.hour.toString()+":"+challengeItem.minutes.toString());
         return convertView;
     }
 }
